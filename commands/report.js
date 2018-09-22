84f27e9518b8f6d1bd4f9362ce3c53c9763b565d
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-09-22 19:04:32 +0200

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,22 +1,23 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 
-  let user = message.mentions.users.first();
+  
   let support = client.channels.find('name', 'staff-modo');
   if (!support) return message.reply('erreur 1');
   let msupport = message.content.split(" ").slice(1);
   message.delete()
  const embed = new Discord.RichEmbed()
   .setColor("RANDOM")
-  .setTitle()
+  .setTitle(`report !`)
   .setDescription(msupport.join(" "))
-  .setAuthor(`${user.username}`)
+  .setAuthor(`${message.author}`)
   .setTimestamp()
 
   return client.channels.get(support.id).sendEmbed(embed);
   client.channels.get(support.id).send("@Modérateur")
 
-  message.channel.send(" :white_check_mark: Votre report à bien était envoyer aux modérateurs !")
+  message.channel.send(" :white_check_mark: Votre report à bien était envoyer aux modérateurs !");
+  
 };
 
   exports.conf = {
