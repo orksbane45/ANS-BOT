0e5aaaab935e7154fcee86b4c545f342ff5e0883
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-11-19 00:02:13 +0100

    Update reglement.js

--- commands/reglement.js
+++ commands/reglement.js
@@ -9,7 +9,7 @@ exports.run = async (client, message, params) => {
     
     let embed = new Discord.RichEmbed()
     .setColor('#2CEF08')
-    .addField("Ⅱ - Conduite :",`**>** Pas de propos/photo/images insultants, vulgaires, racistes, sexistes, religieux, politiques ou homophobes.\r**>** Soyez respectueux, courtois, poli envers les utilisateurs et le staff. "Bonjour", "Merci", "Au revoir" n'ont jamais tué personne.\r**>** Ne dévoilez sous aucun prétexte vos informations de compte, même à un membre du staff !\r**>** Pas de spam (le spam d'émote est aussi considéré comme étant un spam.\r**)`)
+    .addField("Ⅱ - Conduite :",`**>** Pas de propos/photo/images insultants, vulgaires, racistes, sexistes, religieux, politiques ou homophobes.\r**>** Soyez respectueux, courtois, poli envers les utilisateurs et le staff. "Bonjour", "Merci", "Au revoir" n'ont jamais tué personne.\r**>** Ne dévoilez sous aucun prétexte vos informations de compte, même à un membre du staff !\r**>** Pas de spam (le spam d'émote est aussi considéré comme étant un spam.`)
     message.channel.send(embed)
   }
 };
