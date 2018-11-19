a22b4c74811a8440512136c806bf85d2bbd91ee8
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-11-19 14:53:32 +0100

    Create ip.js

---
+++ commands/ip.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+
+
+
+    message.channel.send("**play.anarchynetwork.eu**")
+
+    
+};
+    exports.conf = {
+      enabled: true,
+      guildOnly: false,
+      aliases: [],
+      permLevel: 0
+    };
+
+    exports.help = {
+      name: 'ip',
+      description: 'Donne l'ip du serveur minecraft.',
+      usage: 'ip',
+      aliase: ['ip']
+}
